f7f3ab284954eeb8feafc1b381d7e7ee924368cc
Faiz Raza <muhammadfaizraza48@gmail.com>
2022-08-17 10:43:43 -0700

    final commit

---
+++ script.js
@@ -0,0 +1,88 @@
+console.log('library')
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+
+}
+function Display() {
+
+}
+Display.prototype.add = function (book) {
+    console.log('Adding to UI ')
+    tablebody = document.getElementById('tableBody');
+    let uistring = `<tr>
+                       <td>${book.name}</td>
+                      <td>${book.author}</td>
+                        <td>${book.type}</td> 
+                            </tr>`;
+    tablebody.innerHTML += uistring;
+};
+//implement clear property
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+
+    libraryForm.reset();
+}
+//implement  validate
+Display.prototype.validate = function (book) {
+    let libraryForm = document.getElementById('libraryForm');
+    if (book.name.length < 2 || book.name.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+Display.prototype.show = function (type,displayMessage) {
+
+    let message = document.getElementById('message')
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+<strong>Message</strong>  ${displayMessage}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
+setTimeout(function()
+{
+    message.innerHTML=' '
+},4000); 
+
+}
+
+
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+function libraryFormSubmit(e) {
+
+    // console.log("you have been registered")
+
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {  
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+    console.log(book);
+    let display = new Display()
+    if (display.validate(book)) {
+        display.add(book);
+        display.clear();
+        display.show('success','your book has been succfully added')
+    }
+    else {//something went wrong}
+        display.show('danger','sorry you CANT add this book')
+    }
+    e.preventDefault()
+}
